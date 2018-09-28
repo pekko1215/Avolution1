6dca78627fd976ac1d5e01df1314ccb076bf2638
pekko1215 <s16t218@stu.kagawa-u.ac.jp>
2018-09-28 12:39:56 +0900

    template

--- slot/main.js
+++ slot/main.js
@@ -1,7 +1,41 @@
 class PatiSlot {
 	//メイン基板,サブ基板,リール,リール制御
-	constructor(mainFoundation,subFoundation,reel,reelControl){
-		Object.assign(this,{mainFoundation,subFoundation,reel,reelControl});
+	constructor(mainFoundation,subFoundation,reel,reelControl,save = new SaveData){
+		subFoundation.reel = reel;
+		mainFoundation.reelControl = reelControl;
+		Object.assign(this,{mainFoundation,subFoundation,reel,reelControl,save});
+	}
+	init(){
+
+	}
+	async game(){
+		while(true){
+			await this.bet();
+			await this.lot();
+			await this.lever();
+			for(var i=0;i<3;i++){
+				await this.stop(i);
+			}
+			await this.pay();
+		}
+	}
+	async bet(){
+		var bet = await mainFoundation.bet();
+		await subFoundation.bet(bet);
+	}
+	async lot(){
+		this.lotFlag = this.mainFoundation.lot();
+	}
+	async lever(){
+		await this.subFoundation.lever(this.lotFlag);
+	}
+	async stop(i){
+		var slip = await this.mainFoundation.stop(i);
+		await this.reel.slip(slip);
+	}
+	async pay(){
+		var pay = await this.mainFoundation.pay();
+		await this.subFoundation.pay(pay);
 	}
 }
 
@@ -9,12 +43,19 @@ class MainFoundation {
 	constructor(){
 
 	}
+	bet(){}
+	lot(){}
+	stop(){}
+	pay(){}
 }
 
 class SubFoundation {
 	constructor(){
 
 	}
+	bet(){}
+	lever(){}
+	pay(){}
 }
 
 class Reel {
@@ -25,6 +66,16 @@ class Reel {
 
 class ReelControl {
 
+}
+
+class SaveData {
+	constructor(){
+		this.play = 0;
+		this.allPlay = 0;
+		this.in = 0;
+		this.out = 0;
+		this.bouns = [];
+	}
 }
 
 export {PatiSlot,MainFoundation,SubFoundation,Reel,ReelControl};
